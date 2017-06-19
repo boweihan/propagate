72e030224b0484b1ad2bcc3691b4b595935bcc58
bowei <bowei.han100@gmail.com>
2017-06-18 22:54:03 -0400

    simple pulse animation in landing frame

--- app/Menu.js
+++ app/Menu.js
@@ -8,14 +8,43 @@ import { StyleSheet,
          TouchableHighlight } from 'react-native';
 
 export default class Menu extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      animatedPulse : new Animated.Value(1)
+    };
+    this.pulseAnimationStart = this.pulseAnimationStart.bind(this);
+    this.pulseAnimationEnd = this.pulseAnimationEnd.bind(this);
+  }
+
+  // TODO: change this to be color inversion animation
+  pulseAnimationStart() {
+    let animation = this.state.animatedPulse;
+    animation.setValue(0.2);
+    Animated.timing(animation, {
+      toValue: 1, // fully opaque
+      duration: 1000, // milliseconds
+    }).start(this.pulseAnimationEnd);
+  }
+
+  pulseAnimationEnd() {
+    let animation = this.state.animatedPulse;
+    animation.setValue(1);
+    Animated.timing(animation, {
+      toValue: 0.2, // fully opaque
+      duration: 1000, // milliseconds
+    }).start(this.pulseAnimationStart);
+  }
+
   render() {
+    this.pulseAnimationStart();
     return (
       <View style={styles.menu}>
-        <View style={{flexDirection: 'row', marginBottom: 30}}>
+        <Animated.View style={{flexDirection: 'row', marginBottom: 30, opacity: this.state.animatedPulse}}>
           <Text style={styles.prop} numberOfLines={2}>PROP
             <Text style={styles.agate}>AGATE</Text>
           </Text>
-        </View>
+        </Animated.View>
         {!this.props.firstLoad ?
           <TouchableHighlight underlayColor='white' activeOpacity={0.5}
             onPress={() => this.props.setRoute('game')}>
